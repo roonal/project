b62435619bc66c1d405252e49f25fa8ab0fafe4c
roonal <ronalniraula99@gmail.com>
2019-07-17 11:03:05 +0545

    This is my very first commit

---
+++ application/controllers/home.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+
+		$this->load->helper('url'); // loading url helper for extracting base url while using bootstrap
+	}
+	
+	public function index()
+	{
+		echo "hi";
+		$this->load->view('home_view');
+	}
+
+	
+}
---
+++ application/views/home_view.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+
+<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+ <script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
+ <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+
+
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	
+
