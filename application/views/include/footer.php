b29b6c2be1daf89d28218f9f08bdb7e34ab77cb6
roonal <ronalniraula99@gmail.com>
2019-07-17 16:27:58 +0545

    second changes

---
+++ application/views/include/footer.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+<footer>
+<nav class="navbar1 navbar-default change-nav-footer  navbar-bottom  navbar-light bg-light">
+
+<div class="row foot">
+  <div class="col-sm-4 text-center ">
+    <h2 class="text-bold"> Buy Sell</h2>
+    <p>Bakhundole, Lalitpur</p>
+  </div>
+  
+  <div class="col-sm-4 text-center ">
+    <h2>Navigate</h2>
+    <p><a href="#">Home</a></p>
+    <p><a href="#">About Us</a></p>
+  </div>
+  <div class="col-sm-4 text-center">
+    <h2>Stay Connected With Us</h2>
+    <p><i class="fab fa-facebook-square fa-3x"></i>
+      <i class="fab fa-instagram fa-3x"></i>
+      <i class="fab fa-google-plus-square fa-3x"></i>
+      <i class="fas fa-envelope fa-3x"></i>
+      <i class="fab fa-twitter-square fa-3x"></i>
+    </p>
+  </div>
+</div>
+<p>-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+<div class="text-center footlast"><p>Copyright &copy Buy Sell 2018. All Right Reserved</p>
+<p>Designed By:10 Orbits Pvt Ltd</p></div>
+</nav>
+</footer>
+
+<style>
+	footer{
+
+	    position: fixed;
+	    height: 80px;
+	    bottom: 20px;
+	    width: 100%;
+	    margin-bottom: 105px;
+}
+	.navbar1{
+		background-color: black;
+		color:white;
+	}
+	
+</style>
