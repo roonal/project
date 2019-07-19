92a8de80a15c5506478b7bd31222715c1aa901d8
roonal <ronalniraula99@gmail.com>
2019-07-19 15:41:09 +0545

    my third commit

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,7 +6,20 @@ class Home extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 
-		$this->load->helper('url'); // loading url helper for extracting base url while using bootstrap
+		$this->load->helper("form");//manual loading of the form helper
+
+		$this->load->library("form_validation");
+		//loading of form validation library
+
+		$this->load->helper('url'); // loading url helper for extracting base url while using bootstrap and also for redirect function
+
+		$this->load->database();
+
+		$this->load->library("session");
+		//loading of session for flash messages after form submission
+
+		$this->load->model('form_model');
+		// loading the form model 
 	}
 	
 	public function index()
@@ -16,6 +29,69 @@ class Home extends CI_Controller {
 		$this->load->view('include/footer.php');
 		
 
+	}
+	public function form_submission(){
+
+		$this->form_validation->set_rules("txt_name","name", "required|min_length[6]|max_length[10]|is_unique[contact_form.name]");
+
+		$this->form_validation->set_rules("txt_email","email", "required|is_unique[contact_form.email]");
+
+		$this->form_validation->set_rules("txt_phone","phone", "required|is_unique[contact_form.phone]");
+
+		$this->form_validation->set_rules("txt_message","message", "max_length[100]");
+
+			if($this->form_validation->run()==FALSE){
+				// some errors here
+				$this->session->set_flashdata("error","failed to create user");
+				$this->index();
+
+			}
+			else{
+			
+			$data=$this->input->post();
+			$data_array=array(
+				"name" => $data["txt_name"],
+				"email" => $data["txt_email"],
+				"phone" => $data["txt_phone"],
+				"message" => $data["txt_message"]
+			);
+
+			if ($this->form_model->insert_into_table($data_array)){
+
+				$this->session->set_flashdata("success","Your form has been submitted successfully");
+			 
+			 	redirect('home/dashboard');
+		} 
+		else{
+			$this->session->set_flashdata("error","failed to create user");
+			$this->index();
+
+		}
+			
+
+			
+
+			
+		
+		}
+
+
+
+
+	}
+
+	public function dashboard(){
+		$this->load->view('include/header.php');
+		$this->load->view('include/footer.php');
+
+		echo "<h1> Your fomr has been submitted sucessfully </h1>";
+		echo "<br/>";
+		echo "<br/>";
+		echo "<br/>";
+		echo "<br/>";
+		echo "<br/>";
+		echo "<br/>";
+		echo "<h1> Your fomr has been submitted sucessfully </h1>";
 	}
 
 	
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,3 +1,9 @@
+<style>
+	div.error{
+		color:black;
+	}
+</style>
+
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
@@ -14,6 +20,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<meta charset="utf-8">
 		<title>Welcome to CodeIgniter</title>
 	</head>
+
+	<?php 
+	if($this->session->flashdata("success")){ ?>
+
+		
+			<?php echo $this->session->flashdata("successfully submited your form"); 
+			echo "</br>";
+			 
+			?>
+
+		<?php
+	}
+	?>
+
 	<body >
 		<div class="container">
 			<div class="row">
@@ -22,28 +42,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<strong>Form</strong>
 				</h2>
 				<hr>
-				<form method="post" action="">
+				<?php echo form_open('home/form_submission',array("method"=>"post")); ?>
+
 					<div class="col-md-6">
 						<div class="input-group input-group-lg">
 							<span class="input-group-addon" id="sizing-addon1"><i class="fas fa-user"></i></span>
-							<input type="text" class="form-control" name="fullName" placeholder="Full Name" required>
+							<input type="text" class="form-control" value="<?php echo set_value('txt_name')?>" name="txt_name" placeholder="Full Name"   >
+
+							<?php echo form_error("txt_name","<div class='error'>","</div>"); echo "</br>" ?>
+
 						</div>
 						<div class="input-group input-group-lg">
 							<span class="input-group-addon" id="sizing-addon1"><i class="fas fa-envelope"></i></span>
-							<input type="email" class="form-control" name="email" placeholder="Email" required>
+							<input type="email" class="form-control" value="<?php echo set_value('txt_email')?>" name="txt_email" placeholder="Email"   >
+
+							<?php echo form_error("txt_email","<div class='error'>","</div>"); echo "</br>" ?>
+
 						</div>
 						<div class="input-group input-group-lg">
 							<span class="input-group-addon" id="sizing-addon1"><i class="fas fa-phone"></i></span>
-							<input type="number" class="form-control" name="phone" placeholder="Phone Number" required>
+							<input type="number" class="form-control" value="<?php echo set_value('txt_phone')?>" name="txt_phone" placeholder="Phone Number"  >
+
+							<?php echo form_error("txt_phone","<div class='error'>","</div>"); echo "</br>"?>
+
 						</div>
 					</div>
 					<div class="col-md-6">
 						<div class="input-group">
-							<textarea name="" cols="80" rows="6" class="form-control" placeholder="Your Message"></textarea>
+							<textarea name="txt_message" cols="80" rows="6" class="form-control" value="<?php echo set_value('txt_message')?>" placeholder="Your Message" ></textarea>
 						</div>
 						<button class="btn btn-lg">Submit Your Message</button>
 					</div>
-				</form>
+				<?php echo form_close(); ?> 
 			</div>
 		</div>
 	</body>
