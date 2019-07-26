ed7ebed32c9650e05c98d4d3f9ce92bf930139d3
roonal <ronalniraula99@gmail.com>
2019-07-26 14:41:09 +0545

    committed

---
+++ application/views/home_page.php
@@ -0,0 +1,391 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+	<script src="https://use.fontawesome.com/c0d357e72a.js"></script>
+
+	<link rel="stylesheet" type="text/css" href='http://localhost/ciproject/assets/css/home.css'></link>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 branch_heading">
+					<h2>Featured <b>Products</b></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="item">
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><a href="#"><i class="fa fa-heart-o"></i></a></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/dslr.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>DSLR</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/dslr.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>DSLR</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/dslr.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>DSLR</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/dslr.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>DSLR</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					<h2>New <b>Products</b></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="item">
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/iphone.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>I-Phone 10X</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/iphone.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>I-Phone 10X</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/iphone.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>I-Phone 10X</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/iphone.jpg" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>I-Phone 10X</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Top <b>Sells</b></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="item">
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/mac_book_pro.png" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>Mac Book Pro</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/mac_book_pro.png" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>Mac Book Pro</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/mac_book_pro.png" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>Mac Book Pro</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumb-wrapper">
+							<span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+							<div class="img-box">
+								<img src="http://localhost/ciproject/assets/images/mac_book_pro.png" class="img-responsive img-fluid" alt="dslr_image">
+							</div>
+							<div class="thumb-content">
+								<h4>Mac Book Pro</h4>
+								<div class="star-rating">
+									<ul class="list-inline">
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+								<p class="item-price"><strike>$400.00</strike> <b>$369.00</b></p>
+								<a href="#" class="btn btn-primary">Buy</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="home_image">
+			<h1>Join 12,000+ Buyers and Sellers</h1>
+			<h2>To see your personal recommendations, it takes one minute to register</h2>
+			<a href="#" class="btn btn-primary">Register Here</a>
+					
+		</div>
+
+		<div class="about_company">
+			<h1>What is Buy Sell Nepal ?</h1>
+			<a href="#"><p>Read more about our story</p></a>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<h2>A one-of-a-kind community</h2>
+						<h4>Buy Sell Nepal is a global online marketplace, where people come together to make, sell, buy, and collect unique items.</h4>
+					</div>
+
+					<div class="col-md-4">
+						<h2>Support independent creators</h2>
+						<h4>There’s no Etsy warehouse – just millions of people selling the things they love. We make the whole process easy, helping you connect directly with makers to find something extraordinary.</h4>
+					</div>
+
+					<div class="col-md-4">
+						<h2>Peace of mind</h2>
+						<h4>Your privacy is the highest priority of our dedicated team. And if you ever need assistance, we are always ready to step in for support.</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+
+    </style>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".wish-icon i").click(function(){
+			$(this).toggleClass("fa-heart fa-heart-o");
+		});
+	});	
+</script>
+		<style>
+		 	.home_image{
+				background-image: url("http://localhost/ciproject/assets/images/best.png");
+				background-attachment: fixed;
+				background-repeat: no-repeat;
+				min-height: 300px;
+				width: 100%;
+				background-position: center;
+				background-size: cover;
+				opacity:0.75;
+				}
+			.home_image h1{
+				text-align: center;
+				font-weight: bold;
+				margin-top: 10px;	
+			}
+			
+			.home_image a{
+				width:200px;
+				height:50px;
+				border-radius: 20px;
+				margin-left: 600px;
+				font-size: 20px;
+			}
+			.branch_heading{
+				margin-top: -40px;
+				margin-bottom: -40px;
+			}
+			.about_company{
+				background-image: url("http://localhost/ciproject/assets/images/plain.jpg");
+				background-repeat: no-repeat;
+				min-height: 400px;
+				width: 100%;
+				background-position: center;
+				background-size: cover;
+			}
+			.about_company h1, .about_company p{
+				text-align: center;
+				font-weight: bold;
+				margin-top: 10px;	
+			}
+
+			
+
+		</style>
+	</body>
